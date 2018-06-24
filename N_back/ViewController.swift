b7618ba35d7becc7104e0d5a90e3d0a9e90e5b1c
Ziyao. Chen <ziyao.chen@ziyaos-macbook-air.local>
2018-06-24 15:09:03 -0400

    Initial Commit

---
+++ N_back/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  N_back
+//
+//  Created by Ziyao. Chen on 2018-06-24.
+//  Copyright © 2018 TalentedExplorer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
